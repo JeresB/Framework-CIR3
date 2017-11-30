aa81dc20be6179502d53813c4246c2f49bd5744e
Jeremy <boiselet.jeremy@gmail.com>
2017-11-30 08:37:57 +0100

    add form

--- cir3-TP-symf/src/BackOfficeBundle/Controller/InternauteController.php
+++ cir3-TP-symf/src/BackOfficeBundle/Controller/InternauteController.php
@@ -2,8 +2,15 @@
 
 namespace BackOfficeBundle\Controller;
 
+use BackOfficeBundle\Entity\Internaute;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -31,6 +38,35 @@ class InternauteController extends Controller
             'internaute' => $internaute,
             'annonces' => $annonces,
             'avis' => $avis,
+        ));
+    }
+
+    public function addAction(Request $request) {
+        $internaute = new Internaute();
+
+        $form = $this->createFormBuilder($internaute)
+            ->add('Nom',    TextType::class)
+            ->add('Prenom', TextType::class)
+            ->add('Mail',   EmailType::class)
+            ->add('Tel',    TextType::class)
+            ->add('Actif', CheckboxType::class)
+            ->add('Ajouter',      SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+
+            $em = $this->get('doctrine')->getManager();
+            $em->persist($user);
+            $em->flush($user);
+
+            return $this->redirectToRoute('back_office_listInternaute');
+        }
+
+        return $this->render('BackOfficeBundle:Default:add_internaute.html.twig', array(
+            'form' => $form->createView(),
         ));
     }
 }
